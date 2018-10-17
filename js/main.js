44b91c51cc470f8bfddf384512890ef93aa0ff53
VasyaBilak <vasyabilak.vb@gmail.com>
2018-10-17 18:22:40 +0300

    the same without jquery

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
-//let togle = document.getElementById("myBtn");
-//let hamLi = document.getElementById("hamLi");
-//
-//function myFunction() {
-//    hamLi.style.display = 'block';
-//   }
-//togle.addEventListener("click", myFunction);
+let togle = document.getElementById("myBtn");
+let hamLi = document.getElementById("hamLi");
+
+function myFunction() {
+    togle.classList.toggle("active");
+    hamLi.classList.toggle("open");
+}
+togle.addEventListener("click", myFunction);
 
 
 
-$(document).ready(function(){
-    $('.hamburger').click(function(){
-        $('.hamburger').toggleClass('active')
-        $('.hamLi').style.display = 'block';
-    });
-});
+//$(document).ready(function(){
+//    $('.hamburger').click(function(){
+//        $('.hamburger').toggleClass('active')
+//        $('.hamLi').style.display = 'block';
+//    });
+//});
