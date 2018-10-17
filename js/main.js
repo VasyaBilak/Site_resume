a1c3b37969e667cde9ef1d709eebf4d6bc47547e
VasyaBilak <vasyabilak.vb@gmail.com>
2018-10-17 13:56:24 +0300

    without js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let togle = document.getElementById("myBtn");
+let hamLi = document.getElementById("hamLi");
+
+function myFunction() {
+    hamLi.style.display = 'block';
+}
+togle.addEventListener("click", myFunction);
