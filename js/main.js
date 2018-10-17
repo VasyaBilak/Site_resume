7a09c4059cd54b2d426510c0aac03b74db674c0d
VasyaBilak <vasyabilak.vb@gmail.com>
2018-10-17 18:09:01 +0300

    add jquery

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
-let togle = document.getElementById("myBtn");
-let hamLi = document.getElementById("hamLi");
+//let togle = document.getElementById("myBtn");
+//let hamLi = document.getElementById("hamLi");
+//
+//function myFunction() {
+//    hamLi.style.display = 'block';
+//   }
+//togle.addEventListener("click", myFunction);
 
-function myFunction() {
-    hamLi.style.display = 'block';
-}
-togle.addEventListener("click", myFunction);
+
+
+$(document).ready(function(){
+    $('.hamburger').click(function(){
+        $('.hamburger').toggleClass('active')
+        $('.hamLi').style.display = 'block';
+    });
+});
